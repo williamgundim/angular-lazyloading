b9708fb57def0b48317a80786bb575b4bb725f59
williamgundim <williammatos09@gmail.com>
2021-02-14 22:45:53 -0300

    criado user service

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { TokenService } from '../token/token.service';
+import { User } from './user';
+
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private userSubject = new Subject<User>();
+
+  constructor(
+    private tokenService: TokenService
+  ) {
+
+    //No constructor para notificar caso a pagina sera recarregada e o token já esteja setado.
+    if (this.tokenService.hasToken){
+      this.decodeAndNotify();
+     }
+
+  }
+    
+  setToken(token:string){
+    this.tokenService.setToken(token);
+    this.decodeAndNotify();
+  }
+
+  getUser(){
+    //Criado para os outros componentes conseguirem se inscrever e receber dados do usuário pelo subscribe.  
+    return this.userSubject.asObservable();
+  }
+
+  private decodeAndNotify(){
+
+    let token = this.tokenService.getToken();
+    let user = jwt_decode(token) as User;
+
+    //Emite pelo subject um observavble do tipo user. Os outros componentes que estiverem escritos vão receber a notificação com o novo valor.
+    this.userSubject.next(user);
+
+  }
+
+}
